6321053744275a1554bb51d3594d75b4bb0aaa55
Johns, Erik Michael <erik.johns@student.tugraz.at>
2024-02-05 12:19:18 -0700

    first commit

---
+++ admin-frontend/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HRDC - Maintenance</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacus velit, dictum vitae egestas vel, tincidunt non mauris. Etiam imperdiet dapibus semper. Integer ultrices id ante sagittis vehicula. Integer rhoncus urna nec erat tempor iaculis. Etiam scelerisque, lorem vitae porttitor ornare, ipsum nisl rutrum ex, nec convallis turpis diam quis ex. Proin varius mauris nisi, sed pellentesque diam pharetra nec. Phasellus quam mi, ornare et neque non, interdum aliquet dui.
+
+        Phasellus venenatis mauris quis justo mattis, quis lobortis lectus malesuada. Duis lobortis cursus magna ut facilisis. Nunc at lectus eleifend sapien egestas mattis. Aenean in nisi dui. Cras mattis mollis mauris, quis malesuada lorem varius non. Donec a tincidunt neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+
+        Nam egestas odio non purus mattis posuere. Phasellus risus ex, blandit dapibus metus in, laoreet interdum purus. In leo nibh, posuere vel nisl nec, molestie vestibulum dolor. Aliquam erat volutpat. Proin quis ante a turpis convallis finibus. Aliquam nec velit sit amet ipsum dapibus venenatis non vitae justo. Ut elementum, velit quis sagittis varius, dui mi mattis enim, nec ullamcorper sem orci id augue. Aenean facilisis auctor consectetur. Sed vel mi sit amet velit sagittis mattis. Aenean eget quam quis orci pretium ultrices ut efficitur ipsum.
+
+        In porta eget nisl eget semper. Nunc quis tempus est. Curabitur sollicitudin tortor metus, at dictum ipsum imperdiet sit amet. Aenean volutpat tincidunt dolor, in tincidunt neque accumsan scelerisque. Proin vitae odio et ipsum viverra pharetra at in ligula. Aliquam a congue tellus, nec mattis erat. Nunc eu dui et velit tempor porttitor eget nec turpis.
+
+        Cras a semper ex. Integer sagittis faucibus dictum. Aenean in urna neque. Nulla malesuada facilisis porttitor. Quisque iaculis quis diam a commodo. Phasellus dolor tortor, finibus interdum cursus a, sollicitudin eu velit. Fusce vitae semper ligula. Suspendisse vitae elementum ligula, id pretium nisi. Pellentesque luctus eros in eros tincidunt, sollicitudin condimentum massa posuere. Suspendisse nec laoreet libero, sed tincidunt odio.
+      </p>
+    </div>
+  );
+}
+
+export default App;
