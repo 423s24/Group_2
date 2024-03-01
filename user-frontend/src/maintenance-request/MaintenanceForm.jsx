0308229267a2f120ddcaa14ede9a06703af6aa12
samjacobsen13 <143229760+samjacobsen13@users.noreply.github.com>
2024-03-01 13:11:41 -0700

    maintenance form fields and backend

--- user-frontend/src/maintenance-request/MaintenanceForm.jsx
+++ user-frontend/src/maintenance-request/MaintenanceForm.jsx
@@ -1,13 +1,39 @@
-import {React, useState, useRef} from 'react'
-import "./maintenanceFrom.css"
+import { React, useState, useRef } from 'react';
+import "./maintenanceFrom.css";
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc, doc, setDoc } from 'firebase/firestore';
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD0SfGl4KETc_rGBuiSCDbx9FZk5PzNsnQ",
+  authDomain: "hrdc-maintanance-ticket-mngr.firebaseapp.com",
+  projectId: "hrdc-maintanance-ticket-mngr",
+  storageBucket: "hrdc-maintanance-ticket-mngr.appspot.com",
+  messagingSenderId: "1044310518528",
+  appId: "1:1044310518528:web:5e1f1c3c7bdb8ac7e421e5",
+  measurementId: "G-HK4X8HVEH4"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app)
+
 
 export default function MaintenanceForm() {
   const descriptionRef = useRef();  
   const detailsRef = useRef();
   const areaRef = useRef();   
-  const relatedRef = useRef();  
+  const relatedRef = useRef();
+  const availabilityRef = useRef();
+  const relationRef = useRef();  
+  const buildingRef = useRef();
+  const urgencyRef = useRef();
+  const addressRef = useRef(); 
+  const phoneRef = useRef();
 
   const [description, setDescription] = useState('');
   const [validDescription, setValidDescription] = useState(false);
@@ -26,6 +52,81 @@ export default function MaintenanceForm() {
   const [validDetails, setValidDetails] = useState(false);
   const [detailsFocus, setDetailsFocus] = useState(false);
 
+  const [buildingType, setBuildingType] = useState('');
+  const [validBuildingType, setValidBuildingType] = useState(false);
+  const [buildingTypeFocus, setBuildingTypeFocus] = useState(false);
+
+  const [phone, setPhone] = useState('');
+  const [validPhone, setValidPhone] = useState(false);
+  const [phoneFocus, setPhoneFocus] = useState(false);
+
+  const [availability, setAvailability] = useState('');
+  const [validAvailability, setValidAvailability] = useState(false);
+  const [availabilityFocus, setAvailabilityFocus] = useState(false);
+
+  const [relation, setRelation] = useState('');
+  const [validRelation, setValidRelation] = useState(false);
+  const [relationFocus, setRelationFocus] = useState(false);
+
+  const [validUrgency, setValidUrgency] = useState(false);
+  const [urgencyFocus, setUrgencyFocus] = useState(false);
+
+  const [urgency, setUrgency] = useState(1);
+
+  const [validAddress, setValidAddress] = useState(false);
+  const [addressFocus, setAddressFocus] = useState(false);
+  const [address, setAddress] = useState('');
+
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [validPhoneNumber, setValidPhoneNumber] = useState(false);
+  const [phoneNumberFocus, setPhoneNumberFocus] = useState(false);
+
+  const getAddressKey = (address) => {
+  return address.replace(/\s+/g, '_').toLowerCase();
+ }
+
+
+
+  const handleBuildingTypeChange = (e) => {
+    setBuildingType(e.target.value);
+  };
+
+ 
+  const handleUrgencyChange = (e) => {
+    const value = parseInt(e.target.value, 10);
+    setUrgency(value);
+  };
+
+  const handlePhoneChange = (e) => {
+    const numericValue = e.target.value.replace(/\D/g, '');
+    const formattedValue = formatPhoneNumber(numericValue)
+    setPhone(formattedValue);
+  };
+
+  const formatPhoneNumber = (numericValue) => {
+  const cleaned = ('' + numericValue).replace(/\D/g, '');
+  const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+
+  if (match) {
+    return '(' + match[1] + ') ' + match[2] + '-' + match[3];
+  }
+
+  return numericValue;
+};
+
+  const handleAvailabilityChange = (e) => {
+    setAvailability(e.target.value);
+  };
+
+  const handleRelationChange = (e) => {
+    setRelation(e.target.value);
+  };
+
+  const handleAddressChange = (e) => {
+    setAddress(e.target.value);
+  }
+
+
 
   const handleSubmit = async (e) => {
 
@@ -34,15 +135,100 @@ export default function MaintenanceForm() {
 
      //this is where we would send this information to the database/api
      //printing to console for now
-     console.log(details, description, area, related)
-   }
+  try {
+     const ticketRef = collection(firestore, "ticket");
+     const addressKey = getAddressKey(address);
+
+     await setDoc(doc(ticketRef, addressKey), {
+      description, 
+      area,
+      related, 
+      details, 
+      buildingType, 
+      phone, 
+      availability, 
+      relation, 
+      urgency,
+      address,
+      phone
+     });
+
+     console.log("Data submitted: ", {
+      description, 
+      area,
+      related, 
+      details, 
+      buildingType, 
+      phone, 
+      availability, 
+      relation, 
+      urgency,
+      address,
+      phone,
+     })
+
+     setDescription('');
+     setArea('');
+     setRelated('');
+     setDetails('');
+     setBuildingType('');
+     setPhone('');
+     setAvailability('');
+     setRelated('');
+     setUrgency(1);
+     setAddress('');
+     setPhone('');
+    } catch(error){
+      console.error("Error submitting data: ", error);
+    }
+
+
+   };
 
   return (
 
     <section className='form-section'>
     <h1>Request Maintenance</h1>
     <form onSubmit={handleSubmit}>
-            <div className='input-group wide-input'>
+
+
+          <div className='input-group wide-input'>
+            <input
+              type='text'
+              id='address'
+              ref={addressRef}
+              autoComplete='off'
+              onChange={handleAddressChange}
+              value={address}
+              required
+              aria-invalid={validAddress ? "false" : "true"}
+              onFocus={() => setAddressFocus(true)}
+              onBlur={() => setAddressFocus(false)}
+            />
+            <label htmlFor='address'>
+              Address:
+            </label>
+          </div>
+
+          <div className='input-group wide-input'>
+            <input
+              type='tel'
+              id='phone'
+              ref={phoneRef}
+              autoComplete='off'
+              onChange={handlePhoneChange}
+              value={phone}
+              required
+              aria-invalid={validPhoneNumber ? "false" : "true"}
+              onFocus={() => setPhoneNumberFocus(true)}
+              onBlur={() => setPhoneNumberFocus(false)}
+            />
+            <label htmlFor='phone'>
+              Phone Number:
+            </label>
+          </div>
+
+          <div className='input-group wide-input'>
             <input
                 type="text"
                 id="details"
@@ -60,6 +246,7 @@ export default function MaintenanceForm() {
                 Description:
               </label>
             </div>
+
             <div className='input-group wide-input'>
             <input
                 type="text"
@@ -93,42 +280,180 @@ export default function MaintenanceForm() {
               <label htmlFor="description">Details:</label>
             </div>
             <div className='input-group-radio'>
-            <p>Is this Related to another Request?</p>
-            <div className='radio-pair'>
-              <input
-                  className='radio-input'
-                  type="radio"
-                  id="Yes"
-                  ref={relatedRef}
-                  autoComplete="off"
-                  checked={related === 'Yes'}
-                  onChange={(e) => setRelated(e.target.value)}
-                  value={"Yes"}
-                  required
-                  aria-invalid={validRelated ? "false" : "true"}
-                />
-                <label htmlFor="Yes">
-                  Yes
-                </label>
-              </div>
-              <div className='radio-pair'>  
+              <p>Is this Related to another Request?</p>
+              <div className='radio-pair'>
                 <input
-                  className='radio-input'
-                  type="radio"
-                  id="No"
-                  ref={relatedRef}
-                  autoComplete="off"
-                  checked={related === 'No'}
-                  onChange={(e) => setRelated(e.target.value)}
-                  value={"No"}
-                  required
-                  aria-invalid={validRelated ? "false" : "true"}
-                />
-                <label htmlFor="No">
-                  No
-                </label>
+                    className='radio-input'
+                    type="radio"
+                    id="Yes"
+                    ref={relatedRef}
+                    autoComplete="off"
+                    checked={related === 'Yes'}
+                    onChange={(e) => setRelated(e.target.value)}
+                    value={"Yes"}
+                    required
+                    aria-invalid={validRelated ? "false" : "true"}
+                  />
+                  <label htmlFor="Yes">
+                    Yes
+                  </label>
+                </div>
+                <div className='radio-pair'>  
+                  <input
+                    className='radio-input'
+                    type="radio"
+                    id="No"
+                    ref={relationRef}
+                    autoComplete="off"
+                    checked={related === 'No'}
+                    onChange={(e) => setRelated(e.target.value)}
+                    value={"No"}
+                    required
+                    aria-invalid={validRelated ? "false" : "true"}
+                  />
+                  <label htmlFor="No">
+                    No
+                  </label>
+                </div>
               </div>
-              </div>
+
+            <div className='input-group wide-input'>
+              <input
+                type='text'
+                id='buildingType'
+                ref={buildingRef}
+                autoComplete='off'
+                onChange={handleBuildingTypeChange}
+                value={buildingType}
+                required
+                aria-invalid={validBuildingType ? "false" : "true"}
+                onFocus={() => setBuildingTypeFocus(true)}
+                onBlur={() => setBuildingTypeFocus(false)}
+              />
+              <label htmlFor='buildingType'>
+                Building Type: 
+              </label>
+            </div>    
+
+            <div className='input-group wide-input'>
+              <input
+                type='number'
+                id='urgency'
+                ref={urgencyRef}
+                autoComplete='off'
+                onChange={(e) => {
+                  const value = parseInt(e.target.value, 10);
+                  if(value >= 1 && value <= 10) {
+                    handleUrgencyChange(e);
+                  } else{
+                      urgencyRef.current.value = '';
+                  }
+                }}
+                value={urgency}
+                min={1}
+                max={10}
+                required
+                aria-invalid={validUrgency ? "false" : "true"}
+                onFocus={() => setUrgencyFocus(true)}
+                onBlur={() => setUrgencyFocus(false)}
+              />
+
+              <label htmlFor="urgency">
+                Urgency (Scale from 1-10):
+              </label>
+            </div>
+
+
+            <div className='input-group wide-input tall-input'>    
+              <textarea 
+              name="availability" 
+              id="" 
+              ref={availabilityRef}
+              value={availability}
+              onChange={(e) => setAvailability(e.target.value)}
+              onFocus={() => setAvailabilityFocus(true)}
+              onBlur={() => setAvailabilityFocus(false)}
+              >
+              </textarea>
+              <label htmlFor="availability">
+                Availability:
+              </label>
+            </div>
+
+            <div className='input-group-radio'>
+              <p>Relation to House</p>
+              <div className='radio-pair'>
+                <input
+                    className='radio-input'
+                    type="radio"
+                    id="Building"
+                    ref={relatedRef}
+                    autoComplete="off"
+                    checked={related === 'Building'}
+                    onChange={(e) => setRelated(e.target.value)}
+                    value={"Building"}
+                    required
+                    aria-invalid={validRelated ? "false" : "true"}
+                  />
+                  <label htmlFor="Building">
+                    Building
+                  </label>
+                </div>
+                <div className='radio-pair'>  
+                  <input
+                    className='radio-input'
+                    type="radio"
+                    id="Plumbing"
+                    ref={relatedRef}
+                    autoComplete="off"
+                    checked={related === 'Plumbing'}
+                    onChange={(e) => setRelated(e.target.value)}
+                    value={"Plumbing"}
+                    required
+                    aria-invalid={validRelated ? "false" : "true"}
+                  />
+                  <label htmlFor="Plumbing">
+                    Plumbing
+                  </label>
+                </div>
+                <div className='radio-pair'>  
+                  <input
+                    className='radio-input'
+                    type="radio"
+                    id="Electrical"
+                    ref={relatedRef}
+                    autoComplete="off"
+                    checked={related === 'Electrical'}
+                    onChange={(e) => setRelated(e.target.value)}
+                    value={"Electrical"}
+                    required
+                    aria-invalid={validRelated ? "false" : "true"}
+                  />
+                  <label htmlFor="Electrical">
+                    Electrical
+                  </label>
+                </div>
+                <div className='radio-pair'>  
+                  <input
+                    className='radio-input'
+                    type="radio"
+                    id="Appliance"
+                    ref={relatedRef}
+                    autoComplete="off"
+                    checked={related === 'Appliance'}
+                    onChange={(e) => setRelated(e.target.value)}
+                    value={"Appliance"}
+                    required
+                    aria-invalid={validRelated ? "false" : "true"}
+                  />
+                  <label htmlFor="Appliance">
+                    Appliance
+                  </label>
+                </div>
+              </div>  
+
+
+
 
               <button className='login-button'>Submit</button>
     </form>
