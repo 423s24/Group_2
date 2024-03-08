520d7f3b39926052dbc1a5d321345371d755d0be
Johns, Erik Michael <erik.johns@student.tugraz.at>
2024-03-08 05:22:16 -0700

    Update ticket view

--- admin-frontend/src/Pages/Javascript/ticketView.js
+++ admin-frontend/src/Pages/Javascript/ticketView.js
@@ -1,4 +1,4 @@
-import { Link, Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const TicketInfo = ({ ticket }) => {
 
@@ -12,9 +12,30 @@ const TicketInfo = ({ ticket }) => {
                 <button className="edit-button">Edit</button>
             </div>
 
-            <h2>Ticket Information</h2>
-            <p>Address: {ticket.address}</p>
-            <p></p>
+            <div className="ticket-view-info">
+                <div className="ticket-view-info-inner-section">
+                    <h2>Basic Ticket Information</h2>
+                    <p><strong>Status:</strong> {ticket.status}</p>
+                    <p><strong>Description:</strong> {ticket.description}</p>
+                    <p><strong>Urgency:</strong> {ticket.urgency}</p>
+                    <p><strong>Service Type:</strong> {ticket.serviceType}</p>
+                </div>
+                
+                <div className="ticket-view-info-inner-section">
+                    <h2>Building Information</h2>
+                    <p><strong>Address:</strong> {ticket.address}</p>
+                    <p><strong>Building Type:</strong> {ticket.buildingType}</p>
+                    <p><strong>Area of Building</strong> {ticket.area}</p>
+                    <p><strong>Enter without Tenant Present:</strong> {ticket.enterPerms}</p>
+                </div>
+
+                <div className="ticket-view-info-inner-section">
+                    <h2>Contact Information</h2>
+                    <p><strong>Submitted By:</strong> {ticket.submittedBy}</p>
+                    <p><strong>Phone:</strong> {ticket.phone}</p>
+                    <p><strong>Email:</strong> {ticket.email}</p>
+                </div>
+            </div>
         </div>
     )
 }
