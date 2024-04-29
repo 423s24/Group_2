a16d5b83b7c746cae186dacd694d36208ee98fb2
Johns, Erik Michael <erikj3520@gmail.com>
2024-04-29 09:57:53 -0600

    Added Link to Docs

--- admin-frontend/src/components/Footer.js
+++ admin-frontend/src/components/Footer.js
@@ -1,6 +1,12 @@
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Footer = () => {
     return (
         <div className="footer">
+            <div className="footer-upper-section">
+                <a target="_blank" href="http://docs.hrdcmaintenance.com"><FontAwesomeIcon icon={faQuestionCircle} /> Need Help?</a>
+            </div>
             <p>&copy; 2024 Human Resources Development Council. All Rights Reserved. </p>
         </div>
     )
